bede82cd87ca9b37bb0f8d4f5c5566cf01e1eca9
jbbarth <jeanbaptiste.barth@gmail.com>
2009-12-07 15:06:31 +0100

    Added some validations on Server model

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,6 @@
 class Server < ActiveRecord::Base
   attr_accessible :name, :fqdn, :ipaddress, :description
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_uniqueness_of :fqdn, :case_sensitive => false
+  validates_format_of :name, :with => /\A[a-zA-Z0-9_-]*\Z/
 end

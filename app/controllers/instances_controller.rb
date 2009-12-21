9c6a516773c78783bb8a8c62913e4e3b6356d38e
jbbarth <jeanbaptiste.barth@gmail.com>
2009-12-21 17:34:30 +0100

    Removed useless actions for InstancesController

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,35 +1,6 @@
 class InstancesController < DatacenterPluginController
   before_filter :find_appli
   
-  def index
-    sort_init 'name', 'asc'
-    sort_update %w(id name)
-
-    @instance_count = Instance.count
-    @instance_pages = Paginator.new self, @instance_count, per_page_option, params['page']
-    @instances =  Instance.all :order => sort_clause,
-            :limit  =>  @instance_pages.items_per_page,
-            :offset =>  @instance_pages.current.offset
-
-    render :layout => !request.xhr?
-  end
-  
-  def show
-    @instance = Instance.find(params[:id], :include => :issues)
-    c = ARCondition.new(["instances_issues.instance_id = ?", @instance.id])
-    sort_init([['id', 'desc']])
-    sort_update({'id' => "#{Issue.table_name}.id"})
-    @issue_count = Issue.count(:joins => :instances, :conditions => c.conditions)
-    @issue_pages = Paginator.new self, @issue_count, per_page_option, params['page']
-    @issues = Issue.all :order => 'id desc',
-                        :joins => :instances,
-                        :conditions => c.conditions,
-                        :limit => @issue_pages.items_per_page,
-                        :offset => @issue_pages.current.offset,
-                        :order => sort_clause
-    render :layout => !request.xhr?
-  end
-  
   def new
     @instance = Instance.new
     @servers = Server.active

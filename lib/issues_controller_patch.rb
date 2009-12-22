a7b6ae83e9839e28c23670120d53b66c79494b89
jbbarth <jeanbaptiste.barth@gmail.com>
2009-12-22 14:33:11 +0100

    Refactored a bit issues_controllers_patch

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -3,17 +3,15 @@ require_dependency 'issues_controller'
 class IssuesController
   helper :servers
   helper :applis
-  before_filter :fix_missing_server_ids, :only => [:edit, :update]
-  before_filter :fix_missing_appli_ids, :only => [:edit, :update]
+  before_filter :fix_missing_ids, :only => [:edit, :update]
   
-  def fix_missing_server_ids
+  def fix_missing_ids
     #see railscasts #17 ; actually redmine only uses "edit" method with "post" requests
     #but I don't want this to be broken in future redmine updates, so trying to guess
     #the future :)
-    params[:issue][:server_ids] ||= [] if request.post? || request.put?
-  end
-
-  def fix_missing_appli_ids
-    params[:issue][:appli_ids] ||= [] if request.post? || request.put?
+    if request.post? || request.put?
+      params[:issue][:server_ids] ||= []
+      params[:issue][:appli_ids] ||= []
+    end
   end
 end

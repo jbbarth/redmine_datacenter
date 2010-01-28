5c13b203b01790e7184c5ac41b878093928dfbca
jbbarth <jeanbaptiste.barth@gmail.com>
2010-01-28 12:54:33 +0100

    Tiny refactoring in test/unit/storage_test

--- test/unit/storage_test.rb
+++ test/unit/storage_test.rb
@@ -1,10 +1,13 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class StorageTest < ActiveSupport::TestCase
+  def setup
+    @bay = Storage::Bay.new("DS4300")
+    @bay.load_profile_from_file("test/fixtures/storage/profile_ibm_ds4300.txt")
+  end
+
   def test_bay_creation
-    bay = Storage::Bay.new("DS4300")
-    bay.load_profile_from_file("test/fixtures/storage/profile_ibm_ds4300.txt")
-    assert bay.logical_drives.any?
-    assert bay.arrays.any?
+    assert @bay.logical_drives.any?
+    assert @bay.arrays.any?
   end
 end

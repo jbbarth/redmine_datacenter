95ba45bf8552d3e472306ef95046b4281c0b193d
jbbarth <jeanbaptiste.barth@gmail.com>
2010-02-12 23:10:23 +0100

    Added basic test for Datacenter#*_number methods

--- test/unit/datacenter_test.rb
+++ test/unit/datacenter_test.rb
@@ -1,8 +1,21 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class DatacenterTest < ActiveSupport::TestCase
-  #fixtures :datacenters
+  fixtures :datacenters, :applis, :instances, :servers, :projects
+
   def test_should_be_valid
     assert Datacenter.new(:name => "My Datacenter").valid?
+  end
+
+  def test_applis_number
+    assert_equal 2, Datacenter.find(1).applis_number
+  end
+
+  def test_servers_number
+    assert_equal 2, Datacenter.find(1).applis_number
+  end
+
+  def test_instances_number
+    assert_equal 2, Datacenter.find(1).applis_number
   end
 end

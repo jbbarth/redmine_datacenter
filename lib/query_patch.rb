jbbarth/redmine_datacenter

commit ad45e4ab47738920653f3903f223510d3aea1a0b

Ensured Applis and Servers displayed in a project are in the datacenter of that very project ; TODO: find how to add a validation on Issue#save so that an Issue cannot be saved if Applis/Servers/Instances are not in the good Datacenter

lib/query_patch.rb
@@ -3,13 +3,13 @@ require_dependency 'query'
 class Query
   def available_filters_with_datacenter_custom_filters
     available_filters_without_datacenter_custom_filters
-    if project.nil? || project.module_enabled?(:datacenter)
+    if project.module_enabled?(:datacenter)
       @available_filters["server_id"] = { :type => :list,
                                           :order => 25,
-                                          :values => Server.active.collect{|s| [s.name, s.id.to_s] } }
+                                          :values => Server.for_project(@project.id).active.collect{|s| [s.name, s.id.to_s] } }
       @available_filters["appli_id"] = { :type => :list,
                                           :order => 30,
-                                          :values => Appli.all.collect{|s| [s.name, s.id.to_s] } }
+                                          :values => Appli.for_project(@project.id).collect{|s| [s.name, s.id.to_s] } }
     end
     @available_filters
   end

app/controllers/applis_controller.rb
@@ -6,7 +6,8 @@ class ApplisController < DatacenterPluginController
     sort_update %w(id name)
 
     @status = params[:status] ? params[:status].to_i : Server::STATUS_ACTIVE
-    c = ARCondition.new(@status == 0 ? nil : ["status = ?", @status])
+    c = ARCondition.new(["datacenter_id = ?", @project.datacenter.id])
+    c << ["status = ?", @status] unless @status == 0
     
     unless params[:name].blank?
       name = "%#{params[:name].strip.downcase}%"

app/controllers/servers_controller.rb
@@ -6,7 +6,8 @@ class ServersController < DatacenterPluginController
     sort_update %w(name fqdn description)
     
     @status = params[:status] ? params[:status].to_i : Server::STATUS_ACTIVE
-    c = ARCondition.new(@status == 0 ? nil : ["status = ?", @status])
+    c = ARCondition.new(["datacenter_id = ?", @project.datacenter.id])
+    c << ["status = ?", @status] unless @status == 0
     
     unless params[:name].blank?
       name = "%#{params[:name].strip.downcase}%"

app/models/appli.rb
@@ -17,6 +17,7 @@ class Appli < ActiveRecord::Base
   STATUS_LOCKED = 2
 
   named_scope :active, :conditions => { :status => STATUS_ACTIVE }
+  named_scope :for_project, lambda {|datacenter_id| {:conditions => ["datacenter_id = ?", datacenter_id]}}
   
   def active?
     self.status == STATUS_ACTIVE

app/models/server.rb
@@ -26,6 +26,7 @@ class Server < ActiveRecord::Base
   after_update :save_interfaces
   
   named_scope :active, :conditions => { :status => STATUS_ACTIVE }, :order => 'name asc'
+  named_scope :for_project, lambda {|datacenter_id| {:conditions => ["datacenter_id = ?", datacenter_id]}}
   
   def active?
     self.status == STATUS_ACTIVE

app/views/applis/_issue_form.html.erb
@@ -12,7 +12,7 @@ function toggle_multi_select_datacenter(field) {
 </script>
 <p>
   <label><%= l(:label_appli_plural) %></label>
-  <% applis = Appli.active(:include => :instances) | @issue.applis | @issue.instances.map(&:appli) %>
+  <% applis = Appli.for_project(@project.id).active(:include => :instances) | @issue.applis | @issue.instances.map(&:appli) %>
   <%= select_applis_or_instances(@issue,applis) %>
   <%= link_to_function image_tag('bullet_toggle_plus.png'),
                        "toggle_multi_select_datacenter('issue_appli_instance_ids');",

app/views/instances/_form.html.erb
@@ -18,7 +18,7 @@ function toggle_multi_select_datacenter(field) {
   <p>
   <label><%= l(:field_appli) %><span class="required"> *</span></label>
   <%= collection_select :instance, :appli_id,
-                        Appli.all, :id, :name,
+                        Appli.for_project(@project.id), :id, :name,
                         { :selected => @appli.id, :include_blank => false } %>
   </p>
   <p><%= f.text_field :name, :required => true %></p>

app/views/servers/_issue_form.html.erb
@@ -1,6 +1,6 @@
 <p>
   <label><%= l(:label_server_plural) %></label>
-  <% servers = Server.all(:order => 'name ASC').select{|s| s.active? || @issue.server_ids.include?(s.id)} %>
+  <% servers = Server.for_project(@project.id).all(:order => 'name ASC').select{|s| s.active? || @issue.server_ids.include?(s.id)} %>
   <% if Setting.datacenter_multiselect? %>
     <%= collection_select :issue, :server_ids, servers, :id, :name,
                           { :selected => @issue.server_ids, :include_blank => true },

commit 5c42b5ce4a5528eb3cea2c16f08754ee51abc30d

Fixed: error introduced in commit:ad45e4ab, resulting in a 500 error when trying to access '/issues' (cross-project issues view) : undefined method 'module_enabled?' for nil:NilClass

lib/query_patch.rb
@@ -3,7 +3,7 @@ require_dependency 'query'
 class Query
   def available_filters_with_datacenter_custom_filters
     available_filters_without_datacenter_custom_filters
-    if project.module_enabled?(:datacenter)
+    if project && project.module_enabled?(:datacenter)
       @available_filters["server_id"] = { :type => :list,
                                           :order => 25,
                                           :values => Server.for_project(@project.id).active.collect{|s| [s.name, s.id.to_s] } }
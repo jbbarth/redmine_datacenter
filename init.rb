d10427a5576eb42770d3a142575a1ed0c30eab2d
jbbarth <jeanbaptiste.barth@gmail.com>
2010-03-09 19:31:55 +0100

    Set version to 0.2.1

--- init.rb
+++ init.rb
@@ -22,7 +22,7 @@ Redmine::Plugin.register :datacenter_plugin do
   description 'This plugin helps you manage your (small) datacenter with redmine'
   url 'http://code.jbbarth.com/projects/redmine-datacenter/wiki'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  version '0.3'
+  version '0.2.1'
   requires_redmine :version_or_higher => '0.9.0'
   
   menu :project_menu,

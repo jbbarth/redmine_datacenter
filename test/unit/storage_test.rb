fb1c6b106f1d5aff0a74372bda9cfc05283a2c83
jbbarth <jeanbaptiste.barth@gmail.com>
2010-03-15 17:24:00 +0100

    Adapted storage unit tests (DS4300 only for the moment)

--- test/unit/storage_test.rb
+++ test/unit/storage_test.rb
@@ -2,12 +2,23 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class StorageTest < ActiveSupport::TestCase
   def setup
-    @bay = Storage::Bay.new("DS4300")
-    @bay.load_profile_from_file("test/fixtures/storage/profile_ibm_ds4300.txt")
+    @bays = {}
+    Dir.glob("test/fixtures/storage/*").each do |f|
+      name = File.basename(f)
+      @bays[name] = Storage::Bay.new(name)
+      @bays[name].load_profile_from_file(f)
+    end
   end
 
   def test_bay_creation
-    assert @bay.logical_drives.any?
-    assert @bay.arrays.any?
+    assert_equal [Storage::Bay], @bays.values.map(&:class).uniq
+  end
+
+  def test_arrays_parsing
+    assert_equal 9, @bays["DS4300"].arrays.length
+  end
+
+  def test_logical_drives_parsing
+    assert_equal 98, @bays["DS4300"].logical_drives.length
   end
 end

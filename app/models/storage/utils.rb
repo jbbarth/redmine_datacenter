947970fde74f5e528be074ae64bce4f9d08f3d44
jbbarth <jeanbaptiste.barth@gmail.com>
2010-04-03 20:21:05 +0200

    Improved storage devices parser so that it supports more device types

--- app/models/storage/utils.rb
+++ app/models/storage/utils.rb
@@ -31,10 +31,17 @@ module Storage
     end
 
     def parse_size(line)
-      size = line.gsub(/(\d)[, ](\d)/, '\1\2') #1,234 and 1 234 => 1234
-      return $~[1].to_i if size.match(/(\d+) Bytes/)
+      size = line.gsub(/[^a-z0-9().,]/i, '') #1,234 and 1 234 => 1234 + sanitizes some awful chars
+      if size.match(/\dBytes\)/)
+        size.tr!(",.","")
+        return size.scan(/(\d+)Bytes/).to_s.to_i
+      end
       size.gsub!(/\(.*/,"")
       units = %w(KB MB GB TB PB)
+      #let's try to guess the format :/
+      size.gsub!(",","") if size.include?(",") && size.include?(".")
+      size.gsub!(",","") if size.scan(",").length > 1
+      size.gsub!(",",".")
       num = size.to_f
       units.each_with_index do |u,idx|
         if size.include?(u)
